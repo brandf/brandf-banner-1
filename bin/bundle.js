0b9db04e5a981625428796f95deff96b6890a04d
Brandon Furtwangler <brandon.furtwangler@hotmail.com>
2016-10-06 15:31:48 -0700

    wavy!

--- bin/bundle.js
+++ bin/bundle.js
@@ -17116,7 +17116,7 @@
 /* 305 */
 /***/ function(module, exports) {
 
-	module.exports = "precision mediump float;\n\nuniform vec2 resolution;\nuniform float time;\n\nvoid main() {\n  float checkerSize = 32.0;\n  vec2 uv = gl_FragCoord.xy + vec2(gl_FragCoord.y*0.005 * cos(gl_FragCoord.x*0.1 + 4.0 * time), gl_FragCoord.x*0.005 * cos(gl_FragCoord.y*0.15 + 7.0 * time));\n  vec2 point = (uv.xy + vec2(-10.0 * time, 10.0 * time)) / checkerSize;\n  if (mod(floor(point.x) + floor(point.y), 2.0) < 1.0) {\n    gl_FragColor = vec4(0.28, 0.28, 0.28, 1.0);\n  } else {\n    gl_FragColor = vec4(0.25, 0.25, 0.25, 1.0);\n  }\n}\n"
+	module.exports = "precision mediump float;\n\nuniform vec2 resolution;\nuniform float time;\n\nvoid main() {\n  float checkerSize = 32.0;\n  vec2 uv = gl_FragCoord.xy\n            + vec2(gl_FragCoord.y*0.002 * cos(gl_FragCoord.x*0.1 + 4.0 * time), gl_FragCoord.x*0.0025 * cos(gl_FragCoord.y*0.12 + 7.0 * time))\n            + vec2(2.0 * cos(gl_FragCoord.y*0.3 + 5.0 * time), 3.0 * cos(gl_FragCoord.x*0.22 + 5.0 * time))\n            + vec2(3.0 * cos(gl_FragCoord.x*gl_FragCoord.y*0.00023 + 2.1 * time), 3.0 * cos(gl_FragCoord.x*gl_FragCoord.y*0.00022 + 1.1 * time));\n  vec2 point = (uv.xy + vec2(-10.0 * time, 10.0 * time)) / checkerSize;\n  if (mod(floor(point.x) + floor(point.y), 2.0) < 1.0) {\n    gl_FragColor = vec4(0.27, 0.27, 0.27, 1.0);\n  } else {\n    gl_FragColor = vec4(0.26, 0.26, 0.26, 1.0);\n  }\n}\n"
 
 /***/ }
 /******/ ]);
--- src/checker.frag
+++ src/checker.frag
@@ -5,11 +5,14 @@ uniform float time;
 
 void main() {
   float checkerSize = 32.0;
-  vec2 uv = gl_FragCoord.xy + vec2(gl_FragCoord.y*0.005 * cos(gl_FragCoord.x*0.1 + 4.0 * time), gl_FragCoord.x*0.005 * cos(gl_FragCoord.y*0.15 + 7.0 * time));
+  vec2 uv = gl_FragCoord.xy
+            + vec2(gl_FragCoord.y*0.002 * cos(gl_FragCoord.x*0.1 + 4.0 * time), gl_FragCoord.x*0.0025 * cos(gl_FragCoord.y*0.12 + 7.0 * time))
+            + vec2(2.0 * cos(gl_FragCoord.y*0.3 + 5.0 * time), 3.0 * cos(gl_FragCoord.x*0.22 + 5.0 * time))
+            + vec2(3.0 * cos(gl_FragCoord.x*gl_FragCoord.y*0.00023 + 2.1 * time), 3.0 * cos(gl_FragCoord.x*gl_FragCoord.y*0.00022 + 1.1 * time));
   vec2 point = (uv.xy + vec2(-10.0 * time, 10.0 * time)) / checkerSize;
   if (mod(floor(point.x) + floor(point.y), 2.0) < 1.0) {
-    gl_FragColor = vec4(0.28, 0.28, 0.28, 1.0);
+    gl_FragColor = vec4(0.27, 0.27, 0.27, 1.0);
   } else {
-    gl_FragColor = vec4(0.25, 0.25, 0.25, 1.0);
+    gl_FragColor = vec4(0.26, 0.26, 0.26, 1.0);
   }
 }
